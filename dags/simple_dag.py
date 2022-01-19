c70f7c60bac18f37242261f516ffb01eec32faed
Juanse <juancgomdotpad@gmail.com>
2022-01-19 17:55:35 -0500

    first task and fundamentals

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,4 +1,15 @@
 from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime
 
-with  DAG(dag_id = "simple_dag") as dag:
-    None
+
+with  DAG(
+        dag_id = 'simple_dag',
+        schedule_interval='*/10 * * * *', #datetime's timedelta object can be used to
+        start_date=datetime(2022,1,1),
+        catchup = True, #Backfilling dack runs
+        max_active_runs=2, # Limit parallel dag runs
+        ) as dag:
+    task_1 = DummyOperator(
+        task_id = 'task_1'
+    )
